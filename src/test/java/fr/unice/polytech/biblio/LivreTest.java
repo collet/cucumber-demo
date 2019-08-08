84b9bdd78b0e54bc35bc1146fbdb4bec052f6378
Philippe Collet <Philippe.Collet@unice.fr>
2019-08-08 16:29:19 +0200

    test Junit 5 pour vérifier que c'est bien le moteur JUnit 5 qui exécute update du pom.xml pour Junit 5 + vintage + surefire compatible Junit 5 + cucumber 4.7 (changement d'API depuis 4.5)

---
+++ src/test/java/fr/unice/polytech/biblio/LivreTest.java
@@ -0,0 +1,24 @@
+package fr.unice.polytech.biblio;
+
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LivreTest { // Just pour vérifier que JUnit 5 est bien configuré
+
+    private Bibliotheque biblio;
+    private Livre livre;
+
+    @BeforeEach
+    public void setUp() {
+        biblio = new Bibliotheque();
+        livre = new Livre(biblio);
+    }
+
+    @Test
+    public void EtudiantCreated() {
+
+        assertEquals(false, livre.getEmprunte());
+    }
+
+}
