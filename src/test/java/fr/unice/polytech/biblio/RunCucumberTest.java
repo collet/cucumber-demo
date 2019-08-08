cd1410f8ebc6930217af55cd1a2503bbd3e547db
Philippe Collet <Philippe.Collet@unice.fr>
2019-08-08 16:27:07 +0200

    version anglaise coherente gherkin + stepdefs

---
+++ src/test/java/fr/unice/polytech/biblio/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.biblio;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(value = Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+public class RunCucumberTest { // will run all features found on the classpath in the same package as this class
+}
